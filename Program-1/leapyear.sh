671f37bbdbccda04b326d7d3dbccd2eae2082ba9
Vishal-cs19 <71490243+Vishal-cs19@users.noreply.github.com>
2021-12-08 23:35:56 +0530

    Create leapyear.sh

---
+++ Program-1/leapyear.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Enter the year"
+read year
+if [ `expr $year % 400` -eq 0  ]; then
+echo "$year is a leap year"
+elif [ `expr $year % 100` -eq 0  ]; then
+echo "$year is not a leap year"
+elif [ `expr $year % 4` -eq 0  ]; then
+echo "$year is a leap year"
+else
+echo "$year is not a leap year"
+fi
+© 2021 GitHub, In
